e2e23193c6b49cf93571310c36c54d58534ac403
Jason Noble <github+jasonn@jasonnoble.org>
2016-07-18 21:36:28 -0600

    End of class

---
+++ app/models/customer.rb
@@ -0,0 +1,13 @@
+class Customer < ApplicationRecord
+  before_save :create_account_number
+
+  validates :first_name, :last_name,
+    :phone, :email, presence: true
+
+  private
+
+  def create_account_number
+    return if self.account_number.present?
+    self.account_number = rand(1_000_000) + 999_999_999
+  end
+end
---
+++ spec/models/book_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:author) }
+  it { is_expected.to validate_presence_of(:year) }
+
+  it { is_expected.to validate_numericality_of(:number_of_pages).is_greater_than(10).is_less_than(1000) }
+end
